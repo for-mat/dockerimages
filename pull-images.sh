a06adef91fb9a4bade9dc41691efd23231a18f99
rick <43637488+for-mat@users.noreply.github.com>
2019-10-12 16:33:55 +0800

    Create pull-images.sh

---
+++ pull-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for imagepath in $(cat ./imagepath.txt)
+do
+imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
+docker pull $imagepath
+
+# push到阿里云仓库
+docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/binlog/shang:$imagename
+docker push registry.cn-hangzhou.aliyuncs.com/binlog/shang:$imagename
+
+
